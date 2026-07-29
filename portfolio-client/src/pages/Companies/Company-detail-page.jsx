a314f64ffab2039aa2495ac7182a4b1954359c29
Tillayevxusniddin <tillayevx1@gmail.com>
2026-07-29 15:38:59 +0500

    fix file name

--- portfolio-client/src/pages/Companies/Company-detail-page.jsx
+++ portfolio-client/src/pages/Companies/Company-detail-page.jsx
@@ -6,7 +6,7 @@ import { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { UserContext } from '../../contexts/UserContext'
 import { assignRecruiter, getCompany, unassignRecruiter, updateCompany } from '../../lib/api/companies-api'
-import RecruiterAssignDialog from './Recruiter-assign-dialog'
+import RecruiterAssignDialog from './recruiter-assign-dialog'
 
 const CompanyDetailPage = () => {
 	const { id } = useParams()
